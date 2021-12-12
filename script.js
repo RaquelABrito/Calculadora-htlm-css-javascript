208114e559356da010ca1adf6c5acc3b0b1f50d4
RaquelABrito <raquelbrito662@gmail.com>
2021-12-12 20:50:07 -0300

    funcionando

---
+++ script.js
@@ -0,0 +1,33 @@
+var valoresDigitados = " ";
+function ativa(obj) {
+    //console.log(obj.id);
+    var identificador = obj.id;
+    var valueDiv = document.getElementById(identificador).getAttribute('value');
+
+
+    document.getElementById('visor').innerHTML = valueDiv;
+
+
+
+    if (valueDiv == "-" || valueDiv == "+" || valueDiv == "/" || valueDiv == "*") {
+        if (valueDiv == ",") {
+            valueDiv == ".";
+        }
+    } else {
+        parseFloat(valueDiv);
+        console.log("float" + valueDiv);
+    }
+
+    /*armazenar a string do que foi clicado*/
+    valoresDigitados = valoresDigitados + valueDiv;
+}
+
+function limpar() {
+    document.getElementById('visor').innerHTML = " ";
+    valoresDigitados = "";
+}
+
+function igual() {
+    document.getElementById('visor').innerHTML = eval(valoresDigitados);
+    //console.log(valoresDigitados);
+}
